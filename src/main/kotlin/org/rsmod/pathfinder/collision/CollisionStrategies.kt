2644373cd2a857c31cdf07b75128bdc29503017e
Kris <kristo190@hotmail.com>
2022-05-06 08:22:34 +0300

    Define the roofbound strategy singleton. Update version to 2.1.4.

--- src/main/kotlin/org/rsmod/pathfinder/collision/CollisionStrategies.kt
+++ src/main/kotlin/org/rsmod/pathfinder/collision/CollisionStrategies.kt
@@ -6,6 +6,7 @@ import org.rsmod.pathfinder.flag.CollisionFlag
 
 public object CollisionStrategies {
     public val Normal: CollisionStrategy = NormalBlockFlagCollision()
+    public val RoofBound: CollisionStrategy = RoofBoundFlagCollision()
     public val Swim: CollisionStrategy = InverseBlockFlagCollision(CollisionFlag.FLOOR)
     public val Fly: CollisionStrategy = LineOfSightBlockFlagCollision()
 }
